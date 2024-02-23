9b30399d5abb49ea8550ab16426a77c98f096628
Remy Castro <41156786+RemsGIT@users.noreply.github.com>
2024-02-23 21:30:31 +0100

    Update route.ts cache issue

--- src/app/api/texts/route.ts
+++ src/app/api/texts/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/src/lib/prisma";
 import {NextResponse} from "next/server";
 
+export const revalidate = 0;
+
 export async function GET() {
     const texts = await prisma.personnalize.findMany()
     
