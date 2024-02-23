1b77833c90deebe25e0ef9a9ca60c6331aba0b31
Remy Castro <41156786+RemsGIT@users.noreply.github.com>
2024-02-23 21:43:55 +0100

    Update route.ts

--- src/app/api/partners/route.ts
+++ src/app/api/partners/route.ts
@@ -1,6 +1,8 @@
 import {NextResponse} from "next/server";
 import prisma from "@/src/lib/prisma";
 
+export const revalidate = 0;
+
 export async function GET(){
     const partners = await prisma.partner.findMany({
         include: {
