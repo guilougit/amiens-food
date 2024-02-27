20593b6c1a2fd816005482ffa4956df677f0025c
Remy CASTRO <remycastro27@icloud.com>
2024-02-27 16:09:33 +0100

    remove cache issue instagram

--- src/components/v2/instagram/instagram.tsx
+++ src/components/v2/instagram/instagram.tsx
@@ -1,12 +1,11 @@
 import {GridSlider} from "@/src/components/v2/slider/grid-slider";
 
-
 export default async function InstagramPosts() {
     
     let instagramFetch = null
     let images = null
     if(process.env.INSTAGRAM_KEY) {
-        instagramFetch = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`).then(res => res.json())
+        instagramFetch = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`, { cache: 'no-store' }).then(res => res.json())
 
         images = instagramFetch.data.map((item:any) => ({
             path: item.media_url,
