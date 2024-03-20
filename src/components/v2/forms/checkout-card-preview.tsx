5543a52946cb4c587bb1937ecded4250ae0e2604
Remy CASTRO <remycastro27@icloud.com>
2024-03-20 12:48:00 +0100

    Fix image position on new template checkout

--- src/components/v2/forms/checkout-card-preview.tsx
+++ src/components/v2/forms/checkout-card-preview.tsx
@@ -12,7 +12,7 @@ export const CheckoutCardPreview = () => {
                    className={"rounded-xl"}/>
 
             {/* DATA PREVIEW ON CARD */}
-            {formData.picture && <Image src={URL.createObjectURL(formData.picture)} className={"absolute top-0 left-0 w-[30px] md:w-[70px] "} alt="Photo d'identité" width={200} height={300}/>}
+            {formData.picture && <Image src={URL.createObjectURL(formData.picture)} className={"absolute top-2.5 left-2.5 md:top-3.5 md:left-3.5 w-[80px] h-[80px] md:w-[100px] md:h-[100px] object-cover rounded-full "} alt="Photo d'identité" width={200} height={300}/>}
             <span className={"absolute top-[51%] sm:top-[49.5%] left-[32%] text-xs"}>{formData.firstname}</span>
             <span className={"absolute top-[64.5%] sm:top-[62.8%] left-[28%] text-xs"}>{formData.lastname}</span>
             <span className={"absolute top-[79.5%] sm:top-[77%] left-[32%] text-xs"}>{formData.surname}</span>
