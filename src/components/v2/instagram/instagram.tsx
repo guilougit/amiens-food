0fa5b314b0aa15f1af6f21c3e5b6e84b3284e164
Remy CASTRO <remycastro27@icloud.com>
2024-04-01 18:04:49 +0200

    fix crash

--- src/components/v2/instagram/instagram.tsx
+++ src/components/v2/instagram/instagram.tsx
@@ -1,19 +1,29 @@
 import {GridSlider} from "@/src/components/v2/slider/grid-slider";
 
 
+export const revalidate = 0;
+
 export default async function InstagramPosts() {
     
     let instagramFetch = null
     let images = null
     if(process.env.INSTAGRAM_KEY) {
         instagramFetch = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`, { cache: 'no-store' }).then(res => res.json())
+        
+        
+    
+        console.log(instagramFetch)
+        images = []
+        if(instagramFetch.data) {
+            images = instagramFetch.data.map((item:any) => ({
+                path: item.media_url,
+                caption: item.caption || "",
+                type: item.media_type,
+                link: item.permalink
+            }));
+        }
 
-        images = instagramFetch.data.map((item:any) => ({
-            path: item.media_url,
-            caption: item.caption || "",
-            type: item.media_type,
-            link: item.permalink
-        }));
+
     }
     
     return (
