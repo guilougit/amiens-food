ebf012a9e3a10ccaba38883d09fa7cb80beb68cd
Remy CASTRO <remycastro27@icloud.com>
2024-04-01 18:20:21 +0200

    fix crash

--- src/components/v2/instagram/instagram.tsx
+++ src/components/v2/instagram/instagram.tsx
@@ -8,13 +8,9 @@ export default async function InstagramPosts() {
     let instagramFetch = null
     let images = null
     if(process.env.INSTAGRAM_KEY) {
-        instagramFetch = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`, { cache: 'no-store' }).then(res => res.json())
-        
-        
-    
-        console.log(instagramFetch)
+        instagramFetch = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`, { cache: 'no-store' }).then(res => res.json())
         images = []
-        if(instagramFetch.data) {
+        if(instagramFetch && instagramFetch.data) {
             images = instagramFetch.data.map((item:any) => ({
                 path: item.media_url,
                 caption: item.caption || "",
