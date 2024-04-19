7338ae1aa0e37fc9ff371f48ca98dd969b966d1c
Remy CASTRO <remycastro27@icloud.com>
2024-04-19 19:52:55 +0200

    test compressor image

--- src/utils/compressor.ts
+++ src/utils/compressor.ts
@@ -1,8 +1,8 @@
 import sharp from 'sharp';
 
 export const MAX_IMAGE_WIDTH_PROFILE = 400
-export const MAX_IMAGE_WIDTH = 2000
-export const QUALITY_IMAGE = 80
+export const MAX_IMAGE_WIDTH = 1000
+export const QUALITY_IMAGE = 85
 
 export async function compressAndRoundImage(inputBuffer: Buffer): Promise<Buffer> {
     const roundedBuffer = await sharp(inputBuffer)
@@ -25,13 +25,13 @@ export async function compressImage(inputBuffer: Buffer): Promise<Buffer> {
 
     if (width && width > MAX_IMAGE_WIDTH) {
         roundedBuffer = await sharp(inputBuffer)
-            //.resize(MAX_IMAGE_WIDTH, null, {fit: 'inside'}) 
-            //.jpeg({quality: QUALITY_IMAGE})
+            .resize(MAX_IMAGE_WIDTH, null, {fit: 'inside'}) 
+            .jpeg({quality: QUALITY_IMAGE})
             .toBuffer();
     } else {
         roundedBuffer = await sharp(inputBuffer)
-            //.resize(300, null, {fit: 'inside'})// 300 is less than 600
-            //.jpeg({quality: QUALITY_IMAGE})
+            .resize(300, null, {fit: 'inside'})// 300 is less than 600
+            .jpeg({quality: QUALITY_IMAGE})
             .toBuffer();
     }
 
