edd31c7e950c4c9b84eba7369436cc9e35a29d1f
Remy CASTRO <remycastro27@icloud.com>
2024-04-20 18:59:55 +0200

    change price id for production

--- src/config/prices.ts
+++ src/config/prices.ts
@@ -5,6 +5,6 @@ export const prices = {
     },
     annually: {
         amount: 9.90,
-        stripe_id: "price_1P0oBB01WEMxH4UKTGc26OIF"
+        stripe_id: "price_1P7C6V01WEMxH4UK36vyLtzd" //or dev  : price_1P0oBB01WEMxH4UKTGc26OIF
     }
 }
