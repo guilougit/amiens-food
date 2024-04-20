3856dc557b914968393bd904914501a781804d5f
Remy CASTRO <remycastro27@icloud.com>
2024-04-20 11:16:43 +0200

    test mail debug

--- src/app/api/stripe/webhooks/route.ts
+++ src/app/api/stripe/webhooks/route.ts
@@ -42,12 +42,12 @@ async function handleStripeWebhook(body: any) {
                 })
             }
             catch (error) {
-                return NextResponse.json({ success: false })
                 console.log('error while updating user subscription')
+                return NextResponse.json({ success: false })
             }
             
             // Create card and send by mail
-            fetch(`${process.env.APP_URL}/api/user/card`, {method: 'POST', body: JSON.stringify({afterPayment: true, fromWebhook: true, email: body.data?.object.customer_email})})
+            fetch(`https://amiensfood.com/api/user/card`, {method: 'POST', body: JSON.stringify({afterPayment: true, fromWebhook: true, email: body.data?.object.customer_email})})
                 .then(res => res.json())
                 .then(async res => {
                     // Send it to email with resend
