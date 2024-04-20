6db3cacf983c40a34dd2dddbbf454c563c0d448f
Remy CASTRO <remycastro27@icloud.com>
2024-04-20 10:51:26 +0200

    Add name on partner card slider

--- src/components/v2/partner-card.tsx
+++ src/components/v2/partner-card.tsx
@@ -1,19 +1,19 @@
 import {Card, CardContent, CardHeader, CardTitle} from "@/src/components/ui/card";
 import {Avatar, AvatarFallback, AvatarImage} from "@radix-ui/react-avatar";
 
-export const PartnerCard = ({item}:{item: {title:string, offers: {id: string, text: string}[], logo: string}}) => {
+export const PartnerCard = ({item}:{item: {name:string, offers: {id: string, text: string}[], logo: string}}) => {
     return (
         <Card className={"h-full rounded-3xl"}>
             <CardHeader className={"relative p-4 pb-2 md:-6"}>
                 <Avatar>
                     <AvatarImage
                         className={"absolute rounded-full -top-8 left-1/2 transform -translate-x-1/2 w-[90px] h-[90px] border-2"}
-                        alt={item.title}
+                        alt={item.name}
                         src={`${process.env.NEXT_PUBLIC_AWS_S3_URL_FILE}/${item.logo}`}
                         width={100}
                         height={100}/>
                 </Avatar>
-                <CardTitle className="text-center pt-8">{item.title}</CardTitle>
+                <CardTitle className="text-center pt-10 text-xs text-muted-foreground">{item.name}</CardTitle>
             </CardHeader>
             <CardContent className={"p-2"}>
                 <ul className={"list-disc mx-6"}>
